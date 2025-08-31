d133b38911d1aece8df1696bbc8c07a4b622982a
duraanos <duran364@anadolu.edu.tr>
2025-08-31 12:09:54 +0300

    fix(auth): Correct signIn to use dynamic provider value

--- features/login/lib/auth.ts
+++ features/login/lib/auth.ts
@@ -1,6 +1,6 @@
 import { signIn, signOut } from 'next-auth/react';
 
-const handleSignIn = (provider: string) => signIn('provider');
+const handleSignIn = (provider: string) => signIn(provider);
 const handleSignOut = () => signOut();
 
 export { handleSignIn, handleSignOut };
